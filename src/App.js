a1dc70794ea45c2e477a62effaacb3ebb5d9c0fe
jane <tth17598@gmail.com>
2022-08-31 17:33:53 +0700

    Update: Group component

--- src/App.js
+++ src/App.js
@@ -6,26 +6,35 @@ function App() {
   console.log('App rendered');
   const [data] = useState([
     {
-      id: 1,
-      type: 'text-field',
-      label: 'First Name',
-      autoComplete: 'off',
-      dependency: {
-        field_id: 2,
-        value: true,
-      }
-    },
-    {
-      id: 2,
+      id: 6,
       type: 'check-box',
-      label: 'Checkbox',
+      label: 'First',
       have_dependency: true,
+      isHalf: true,
     },
     {
-      id: 3,
+      id: 7,
+      type: 'check-box',
+      label: 'Last',
+      have_dependency: true,
+      isHalf: true,
+    },
+    {
+      id: 8,
       type: 'text-style',
       variation: 'code',
       children: 'Hello World',
+    },
+    {
+      id: 2,
+      type: 'text-field',
+      label: 'Last Name',
+      autoComplete: 'off',
+      dependency: {
+        field_id: 7,
+        value: false,
+      },
+      isHalf: true,
     },
     {
       id: 3,
@@ -42,13 +51,24 @@ function App() {
       id: 5,
       type: 'check-box',
       label: 'ABC',
-    }
+    },
+    {
+      id: 1,
+      type: 'text-field',
+      label: 'First Name',
+      autoComplete: 'off',
+      dependency: {
+        field_id: 6,
+        value: false,
+      },
+      isHalf: true,
+    },
   ]);
   const [formData, setFormData] = useState([]);
 
   useEffect(() => {
     const dataFormat = data.map(item => {
-      const { id, type, dependency, have_dependency, children, ...props } = item;
+      const { id, type, dependency, have_dependency, children, isHalf, ...props } = item;
       return {
         id,
         type,
@@ -56,6 +76,7 @@ function App() {
         have_dependency,
         children,
         props,
+        isHalf
       }
     });
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,6 @@ import Element from './Element';
 import { Stack } from '@shopify/polaris';
 
 function Form({ data }) {
-  console.log('Form render');
   const [dependencyValue, setDependencyValue] = useState({});
 
   const changeDependencyValue = useCallback((id, value) => {
@@ -13,28 +12,51 @@ function Form({ data }) {
     }));
   }, []);
 
-  return (
-    <Stack vertical>
-      {data && data.map(
-        ({ id, type, props, children, have_dependency, dependency }) => {
-          const extraProps = {
-            ...(have_dependency && { changeDependencyValue }),
-            ...(dependency && { isHidden: !dependencyValue[dependency.field_id] })
-          };
+  const mapElement = useCallback((item) => {
+    const { id, type, props, children, have_dependency, dependency } = item;
+    const extraProps = {
+      ...(have_dependency && { changeDependencyValue }),
+      ...(dependency && { isHidden: !dependencyValue[dependency.field_id] })
+    };
 
-          return (
-            <Element
-              key={id}
-              id={id}
-              type={type}
-              props={props}
-              children={children}
-              {...extraProps}
-            />
-          )
-        }
-      )}
-    </Stack>
+    return (
+      <Element
+        key={id}
+        id={id}
+        type={type}
+        props={props}
+        children={children}
+        {...extraProps}
+      />
+    )
+  }, [changeDependencyValue, dependencyValue]);
+
+  return (
+    <>
+      <Stack vertical>
+        {data && data.reduce((res, el) => {
+          if (el.isHalf) {
+            if (Array.isArray(res[res.length - 1])) res[res.length - 1].push(el);
+            else res.push([el]);
+          } else {
+            res.push(el);
+          }
+
+          return res;
+        }, []).map(items => {
+            if (Array.isArray(items)) {
+              return (
+                <Stack distribution='equalSpacing'>
+                  {items.map(item => mapElement(item))}
+                </Stack>
+              )
+            }
+
+            return mapElement(items);
+          }
+        )}
+      </Stack>
+    </>
   );
 }
 
